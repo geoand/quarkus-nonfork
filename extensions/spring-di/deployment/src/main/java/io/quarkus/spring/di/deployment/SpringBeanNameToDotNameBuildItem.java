6036566bf011bd0e3cbd3ee057b1d8924d15d44e
Georgios Andrianakis <geoand@gmail.com>
2020-01-20 14:15:13 +0200

    Remove comments

--- extensions/spring-di/deployment/src/main/java/io/quarkus/spring/di/deployment/SpringBeanNameToDotNameBuildItem.java
+++ extensions/spring-di/deployment/src/main/java/io/quarkus/spring/di/deployment/SpringBeanNameToDotNameBuildItem.java
@@ -6,10 +6,7 @@ import org.jboss.jandex.DotName;
 
 import io.quarkus.builder.item.SimpleBuildItem;
 
-/**
- * The purpose of this bean is to map the names of the Spring Beans to their associated DotName
- * This info is needed when trying to convert SpEL expressions that reference beans by name, to bytecode
- */
+
 public final class SpringBeanNameToDotNameBuildItem extends SimpleBuildItem {
 
     private final Map<String, DotName> map;
